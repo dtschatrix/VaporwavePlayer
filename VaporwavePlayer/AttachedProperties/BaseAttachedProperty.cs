cc3a4fecd21db3038b29cb596aa2e7dab9ce18ca
dtschatrix <dtschatrix@yahoo.com.ua>
2019-09-14 16:58:58 +0300

    Added attached properties. Custom passwordBox.

---
+++ VaporwavePlayer/AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+
+namespace VaporwavePlayer
+{
+    /// <summary>
+    /// base attached property 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="Parent">The parent class to be the attached property</typeparam>
+    /// <typeparam name="Property">The type of this attached property</typeparam>
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent : BaseAttachedProperty<Parent,Property>, new()
+    {
+
+        #region Public Events
+
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+
+
+        #endregion
+
+        #region Public Properties
+
+        public static Parent Instance { get;  private set; } = new Parent();
+
+        #endregion
+
+        #region Attached Property Definintions
+        /// <summary>
+        /// The attached property for this class
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty = 
+            DependencyProperty.RegisterAttached("Value", typeof(Property),typeof(BaseAttachedProperty<Parent, Property>),new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+        /// <summary>
+        /// The callback event when <see cref="ValueProperty"/> is changed
+        /// </summary>
+        /// <param name="d">The UI element had it's property changed</param>
+        /// <param name="e">The arguments for the event</param>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Instance.OnValueChanged(d, e);
+
+            //Call event listeners 
+            Instance.ValueChanged(d, e);
+        }
+        /// <summary>
+        /// Gets the value of attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <returns></returns>
+        public static Property GetValue(DependencyObject d) => (Property) d.GetValue(ValueProperty);
+        /// <summary>
+        /// Sets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <param name="value">The value to set property to</param>
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+
+        #endregion
+
+        #region Event Methods
+        /// <summary>
+        /// This method thas is called when any attached property of this type is changed
+        /// </summary>
+        /// <param name="sender">The UI element that this property was changed for</param>
+        /// <param name="e">The arguments for this event</param>
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) { }
+
+
+        #endregion
+    }
+
+    
+}
