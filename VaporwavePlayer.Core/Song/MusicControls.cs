46ad9c3db8fd1fe2e6de10833f2dce9e9ea6a0aa
dtschatrix <clayman@firemail.cc>
2019-10-19 12:55:08 +0300

    added minor changes, UI for control media panel.

--- VaporwavePlayer.Core/Song/MusicControls.cs
+++ VaporwavePlayer.Core/Song/MusicControls.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Documents;
 using CSCore;
 using CSCore.Codecs;
 using CSCore.CoreAudioAPI;
